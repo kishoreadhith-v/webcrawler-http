1fdd200c45063fbf4fee9951818e40f8761d5369
kishoreadhith-v <karthi@pop-os.localdomain>
2023-03-20 22:03:20 +0530

    crawl page recursive working

--- crawl.js
+++ crawl.js
@@ -1,25 +1,45 @@
 const {JSDOM} = require('jsdom')
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+    
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+    if (baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages
+    }
+
+    const normalisedCurrentURL = normalizeURL(currentURL)
+    if (pages[normalisedCurrentURL] > 0) {
+        pages[normalisedCurrentURL]++
+        return pages
+    }
+
+    pages[normalisedCurrentURL] = 1
     console.log(`actively crawling: ${currentURL}`);
 
     try {
         const resp = await fetch(currentURL)
         if (resp.status > 399) {
             console.log(`error in fetch with status code: ${resp.status} on page ${currentURL}`);
-            return
+            return pages
         }
 
         const contentType = resp.headers.get('content-type')
         if (!contentType.includes('text/html')) {
             console.log(`not HTML, content type: ${contentType} on page ${currentURL}`);
-            return
+            return pages
         }
 
-        console.log(await resp.text());
+        const htmlBody = await resp.text()
+        nextURLs = getURLsFromHTML(htmlBody, baseURL)
+        for (const nextURL of nextURLs) {
+            pages = await crawlPage(baseURL, nextURL, pages)
+        }
+
     } catch (err) {
         console.log(`error in fetch: ${err.message} in the page ${currentURL}`);
     }
+    return pages
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const {crawlPage} = require('./crawl.js')
 
-function main() {
+async function main() {
     if (process.argv < 3) {
         console.log("No website entered");
         process.exit(1)
@@ -12,6 +12,9 @@ function main() {
 
     const baseURL = process.argv[2]
     console.log(`starting crawl of ${baseURL}`)
-    crawlPage(baseURL)
+    const pages = await crawlPage(baseURL, baseURL, {})
+    for (const page of Object.entries(pages)) {
+        console.log(page);
+    }
 }
 main()
